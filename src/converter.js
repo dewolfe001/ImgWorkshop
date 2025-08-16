ae983131425aff44372074ac29255fb200cd3733
Shawn DeWolfe <shawn@shawndewolfe.com>
2025-08-16 14:44:54 -0700

    Add circle crop and image filters

--- src/converter.js
+++ src/converter.js
@@ -1,6 +1,14 @@
-export function convert(canvas, format, quality) {
+export function convert(canvas, format, quality, filter) {
     return new Promise((resolve) => {
-        canvas.toBlob((blob) => {
+        const off = document.createElement('canvas');
+        off.width = canvas.width;
+        off.height = canvas.height;
+        const ctx = off.getContext('2d');
+        if (filter) {
+            ctx.filter = filter;
+        }
+        ctx.drawImage(canvas, 0, 0);
+        off.toBlob((blob) => {
             resolve(blob);
         }, format, quality);
     });
